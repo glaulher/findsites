95e153430251fe143e11273084ac022477287682
glaulher <glaulher.developer@gmail.com>
2024-12-19 20:17:52 -0300

    feat: update main sites Tim and expo 52

--- src/components/SiteCard/index.tsx
+++ src/components/SiteCard/index.tsx
@@ -1,5 +1,7 @@
 import React, { ReactNode } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Alert, TouchableOpacity } from 'react-native';
+
+import * as Clipboard from 'expo-clipboard';
 
 import Icon from 'react-native-vector-icons/Feather';
 import { iconMap, styles } from './styles';
@@ -25,32 +27,51 @@ export function SiteCard({
   longitude,
   type,
 }: Props) {
+  const copyToClipboard = (
+    site: string,
+    address: string,
+    city: string,
+    region: string,
+    lat: string,
+    long: string,
+  ) => {
+    const locationData = `Site: ${site},\nEndereço: ${address},\nCidade: ${city},\nRegião: ${region},\nLatitude: ${lat},\nLongitude: ${long}`;
+    Clipboard.setStringAsync(locationData);
+    Alert.alert(`${site}`, 'Endereço copiado!');
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.cardWrapper}>
-        <Text style={styles.title}>{site}</Text>
-
-        {children}
-      </View>
-
-      <Text style={styles.address}>{address}</Text>
-      <View style={styles.city}>
-        <Text style={styles.text}>{city}</Text>
+      <TouchableOpacity
+        onPress={() =>
+          copyToClipboard(site, address, city, region, latitude, longitude)
+        }
+      >
         <View style={styles.cardWrapper}>
-          <Text style={styles.text}>{region}</Text>
-          <Text style={styles.text}>Infra: {type}</Text>
+          <Text style={styles.title}>{site}</Text>
+
+          {children}
         </View>
-      </View>
-      <View style={styles.footer}>
-        <View style={styles.coordinates}>
-          <Icon color={iconMap} name="map-pin" />
-          <Text style={styles.latitude}>latitude:{latitude}</Text>
+
+        <Text style={styles.address}>{address}</Text>
+        <View style={styles.city}>
+          <Text style={styles.text}>{city}</Text>
+          <View style={styles.cardWrapper}>
+            <Text style={styles.text}>{region}</Text>
+            <Text style={styles.text}>Infra: {type}</Text>
+          </View>
         </View>
-        <View style={styles.coordinates}>
-          <Icon color={iconMap} name="map-pin" />
-          <Text style={styles.longitude}>longitude:{longitude}</Text>
+        <View style={styles.footer}>
+          <View style={styles.coordinates}>
+            <Icon color={iconMap} name="map-pin" />
+            <Text style={styles.latitude}>latitude:{latitude}</Text>
+          </View>
+          <View style={styles.coordinates}>
+            <Icon color={iconMap} name="map-pin" />
+            <Text style={styles.longitude}>longitude:{longitude}</Text>
+          </View>
         </View>
-      </View>
+      </TouchableOpacity>
     </View>
   );
 }
